8471b130965c50ee49b3715d560348bee4116500
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-11-19 11:48:54 +0900

    add webpackの上書きを削除

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-    config.plugins.push(new webpack.IgnorePlugin(/\.stor(ies|y).[tj]sx$/))
-    return config
-  },
 }
 
 module.exports = nextConfig
