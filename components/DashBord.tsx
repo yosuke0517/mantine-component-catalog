08c43d10f05c73c1351fe3339d6254d3932859bd
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-11-21 19:36:56 +0900

    add ドロップダウンメニューを作成、メニュー内に各ページへのリンクを設置

--- components/DashBord.tsx
+++ components/DashBord.tsx
@@ -3,6 +3,8 @@ import { LogoutIcon } from '@heroicons/react/outline'
 import { ActionIcon, Center, Menu } from '@mantine/core'
 import { supabase } from '../utils/supabase'
 import { Layout } from '../components/Layout'
+import { Settings } from 'tabler-icons-react'
+import { NextLink } from '@mantine/next'
 
 export const DashBord = ({}) => {
   const signOut = () => {
@@ -20,6 +22,28 @@ export const DashBord = ({}) => {
       <Center>
         <ShieldCheckIcon className="mb-4 h-14 w-14 text-teal-500" />
       </Center>
+      <Center>
+        <Menu trigger="hover" size="xl">
+          <Menu.Label>UI Components</Menu.Label>
+          <Menu.Item component="a" href="/button" icon={<Settings size={16} />}>
+            Button
+          </Menu.Item>
+          <Menu.Item component="a" href="/grid" icon={<Settings size={16} />}>
+            Grid
+          </Menu.Item>
+          <Menu.Item component="a" href="/group" icon={<Settings size={16} />}>
+            Group
+          </Menu.Item>
+          <Menu.Item
+            component="a"
+            href="/multi-select"
+            icon={<Settings size={16} />}
+          >
+            MultiSelect
+          </Menu.Item>
+        </Menu>
+      </Center>
+
       <Center>
         <ActionIcon my="md" size="lg" onClick={signOut}>
           <LogoutIcon />
