4a74a37af62d750fd9442de8162252db443080b8
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-11-23 17:44:23 +0900

    fix storybookをダークモードへ変更

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  docs: {
+    theme: themes.dark,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
