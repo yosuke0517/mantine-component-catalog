5fdbfd7bed643e4a65111abda6886a6cf9af5ada
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-11-23 14:00:54 +0900

    fix 背景色のpropsを追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,21 +2,27 @@ import { FC, ReactNode } from 'react'
 import Head from 'next/head'
 
 type Props = {
-    title: string
-    children: ReactNode // ラップされたコンポーネントを受け取る
+  title: string
+  bg?: string
+  children: ReactNode // ラップされたコンポーネントを受け取る
 }
 
-export const Layout: FC<Props> = ({ children, title = 'Mantine' }) => {
-    return (
-        <div className="flex min-h-screen">
-            <Head>
-                <title>{title}</title>
-            </Head>
-            <header></header>
-            <main className="flex flex-1 flex-col justify-center p-4">
-                {children}
-            </main>
-            <footer></footer>
-        </div>
-    )
+export const Layout: FC<Props> = ({
+  children,
+  bg = 'bg-black',
+  title = 'Mantine',
+}) => {
+  const style = `flex min-h-screen ${bg}`
+  return (
+    <div className={style}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header></header>
+      <main className="flex flex-1 flex-col justify-center p-4">
+        {children}
+      </main>
+      <footer></footer>
+    </div>
+  )
 }
