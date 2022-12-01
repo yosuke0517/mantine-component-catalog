e47a03edf550e164bc534b5c4a886d409a693a32
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-12-01 20:27:31 +0900

    fix Button形式へ変更

--- components/DashBord.tsx
+++ components/DashBord.tsx
@@ -1,11 +1,12 @@
 import { ShieldCheckIcon } from '@heroicons/react/solid'
 import { LogoutIcon } from '@heroicons/react/outline'
-import { ActionIcon, Center, Menu } from '@mantine/core'
+import { useRouter } from 'next/router'
+import { ActionIcon, Button, Center, Group } from '@mantine/core'
 import { supabase } from '../utils/supabase'
 import { Layout } from './Layout'
-import { Settings } from 'tabler-icons-react'
 
 export const DashBord = ({}) => {
+  const router = useRouter()
   const signOut = () => {
     // TODO フラッシュメッセージにする
     supabase.auth
@@ -21,50 +22,44 @@ export const DashBord = ({}) => {
       <Center>
         <ShieldCheckIcon className="mb-4 h-14 w-14 text-teal-500" />
       </Center>
-      <Center>
-        <Menu trigger="hover" size="xl">
-          <Menu.Label>UI Components</Menu.Label>
-          <Menu.Item component="a" href="/button" icon={<Settings size={16} />}>
-            Button
-          </Menu.Item>
-          <Menu.Item component="a" href="/grid" icon={<Settings size={16} />}>
-            Grid
-          </Menu.Item>
-          <Menu.Item component="a" href="/group" icon={<Settings size={16} />}>
-            Group
-          </Menu.Item>
-          <Menu.Item
-            component="a"
-            href="/multi-select"
-            icon={<Settings size={16} />}
-          >
-            MultiSelect
-          </Menu.Item>
-          <Menu.Item
-            component="a"
-            href="/fetch-api"
-            icon={<Settings size={16} />}
-          >
-            Fetch Api（Loading）
-          </Menu.Item>
-          <Menu.Item component="a" href="/modal" icon={<Settings size={16} />}>
-            Modal Demo
-          </Menu.Item>
-          <Menu.Item component="a" href="/avatar" icon={<Settings size={16} />}>
-            Avatar Demo
-          </Menu.Item>
-          <Menu.Item component="a" href="/card" icon={<Settings size={16} />}>
-            Card Demo
-          </Menu.Item>
-          <Menu.Item
-            component="a"
-            href="/match-rate"
-            icon={<Settings size={16} />}
-          >
-            Match Rate Demo
-          </Menu.Item>
-        </Menu>
-      </Center>
+      <Group>
+        <Button color="gray" onClick={() => router.push('/button')}>
+          Button Demo
+        </Button>
+        <Button color="red" onClick={() => router.push('/grid')}>
+          Grid Demo
+        </Button>
+        <Button color="pink" onClick={() => router.push('/group')}>
+          Group Demo
+        </Button>
+        <Button color="grape" onClick={() => router.push('/multi-select')}>
+          MultiSelect Demo
+        </Button>
+        <Button color="violet" onClick={() => router.push('/fetch-api')}>
+          FetchApi （Loading） Demo
+        </Button>
+        <Button color="indigo" onClick={() => router.push('/modal')}>
+          Modal Demo
+        </Button>
+        <Button color="blue" onClick={() => router.push('/avatar')}>
+          Avatar Demo
+        </Button>
+        <Button color="cyan" onClick={() => router.push('/card')}>
+          Card Demo
+        </Button>
+        <Button color="teal" onClick={() => router.push('/match-rate')}>
+          MatchRate Demo
+        </Button>
+        <Button color="green" onClick={() => router.push('/crypt')}>
+          Crypt Demo
+        </Button>
+        <Button color="lime" onClick={() => router.push('/notification')}>
+          Notification Demo
+        </Button>
+        <Button color="yellow" onClick={() => router.push('/hooks')}>
+          Hooks Demo
+        </Button>
+      </Group>
 
       <Center>
         <ActionIcon my="md" size="lg" onClick={signOut}>
