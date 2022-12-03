2e9780693fd3087ceaadf39c1e83ab5c08660f04
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-12-03 12:42:50 +0900

    fix ゴーストのstyleを修正・他IDEの指摘を修正

--- pages/draggable.tsx
+++ pages/draggable.tsx
@@ -1,12 +1,12 @@
 import { NextPage } from 'next'
-import { Group } from '@mantine/core'
+import { Center, Group, Loader } from '@mantine/core'
 import { DraggableY } from '../components/DraggableY'
 import { Todo } from '../types'
 import { useQueryTodos } from '../hooks/useQueryTodos'
 import { Layout } from '../components/Layout'
 
 export const DraggableDemo: NextPage = ({}) => {
-  const { data } = useQueryTodos()
+  const { data, status } = useQueryTodos()
 
   const changeItem = (item: Todo[]) => {
     // TODO update priority
@@ -15,6 +15,11 @@ export const DraggableDemo: NextPage = ({}) => {
   }
   return (
     <Layout title="draggable" bg="bg-gray-200">
+      {status === 'loading' && (
+        <Center>
+          <Loader color="indigo" size="md" variant="bars" />
+        </Center>
+      )}
       <Group>
         {data && (
           <DraggableY
