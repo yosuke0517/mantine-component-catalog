72572f0453eb58348c44dbdd49528a2a4ff4bd43
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-12-03 11:12:09 +0900

    fix 不要なソース削除

--- pages/draggable.tsx
+++ pages/draggable.tsx
@@ -1,10 +1,8 @@
 import { NextPage } from 'next'
 import { Group } from '@mantine/core'
 import { Draggable } from '../components/Draggable'
-import { useEffect, useState } from 'react'
 import { Todo } from '../types'
 import { useQueryTodos } from '../hooks/useQueryTodos'
-import { supabase } from '../utils/supabase'
 import { Layout } from '../components/Layout'
 
 export const DraggableDemo: NextPage = ({}) => {
